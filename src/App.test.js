09c9b6edcc00cdd68d5b75d796c8ac1d1f3f12fa
Guru prasath M <69638057+guru242@users.noreply.github.com>
2025-02-01 13:50:24 +0530

    Add files via upload

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("renders the homepage text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/Hi, I'm Guru Prasath/i); // ✅ Updated text
+  expect(textElement).toBeInTheDocument();
 });
